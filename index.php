8e0d814ef18ce56a450b72a1fa7f96142efd2159
Georgy <goosebumpsmail2@gmail.com>
2020-03-07 18:17:21 +0300

    сделал функцию поиска перв. корней

--- index.php
+++ index.php
@@ -1,24 +1,71 @@
 <?php
 
-define("iterCountOfFermCheck", 1000);
-
+//define("iterCountOfFermCheck", 1000);
 do {
 
-    $simpleNumber = mt_rand(2049, 10_000_000);
-    $isSimple = getSimpleNumber($simpleNumber);
+//    $simpleNumber = mt_rand(2049, 10000000);
+    $simpleNumber = mt_rand(3, 2049);
+    $isSimple = isSimpleNumber($simpleNumber);
 
 } while (!$isSimple);
 
-var_dump($simpleNumber);
+echo getPrimRoot(101);
 
+function _nod($a, $b)
+{
 
-function getSimpleNumber($rndNumberSimple)
+    if ($b == 0)
+        return $a;
+    else
+        return _nod($b, $a % $b);
+
+}
+
+function _phi($n)//функция эйлера
+{
+
+    $resArr = [];
+    for ($i = 1; $i < $n; $i++) {
+
+        if (_nod($i, $n) == 1)
+            array_push($resArr, $i);
+    }
+
+//    var_dump($resArr);
+
+    return count($resArr);
+
+}
+
+function getPrimRoot($m)//они есть, теорема гауса
+{
+    $flag = 0;
+    $g = 0;
+    for (; ;) {
+        $g++;
+        $phi = _phi($m);
+
+        if (bcpowmod($g, $phi, $m))
+            for ($i = 1; $i < $phi; $i++)
+                if (bcpowmod($g, $i, $m) == 1)
+                    continue 2;
+                else
+                    $flag = 1;
+
+        if ($flag)
+            return $g;
+
+    }
+
+}
+
+function isSimpleNumber($rndNumberSimple)//пытался сделать на тесте ферма
 {
     if ($rndNumberSimple % 2 == 0)
         return false;
 
     $i = 3;
-    while ($i * $i <= $rndNumberSimple){
+    while ($i * $i <= $rndNumberSimple) {
 
         if ($rndNumberSimple % $i === 0)
             return false;
